357449ef2e2fd999c6f52706588bc527b255f741
Victor Wondermega <victorwondermega@gmail.com>
2022-07-12 11:34:54 +0500

    before build

--- core/components/zagata/elements/snippets/snippet.zagata.php
+++ core/components/zagata/elements/snippets/snippet.zagata.php
@@ -1,6 +1,17 @@
 <?php
 
-  $doodles = $modx->getService('doodles','Doodles',$modx->getOption('zagata.core_path',null,$modx->getOption('core_path').'components/zagata/').'model/zagata/',$scriptProperties);
-  if(!($doodles instanceof Doodles)) return '';
+  $za = $modx->getService('zagata','Zagata',$modx->getOption('zagata.core_path',null,$modx->getOption('core_path').'components/zagata/').'model/zagata/',$scriptProperties);
+
+  if(!($za instanceof Zagata)) return '';
+
+ 
+  /* setup default properties */
+  $tpl = $modx->getOption('tpl',$scriptProperties,'rowTpl');
+  $sort = $modx->getOption('sort',$scriptProperties,'name');
+  $dir = $modx->getOption('dir',$scriptProperties,'ASC');
+ 
+  $output = '';
+ 
+  return $output;
 
 ?>
---
+++ core/components/zagata/model/zagata/zagata.class.php
@@ -0,0 +1,25 @@
+<?php
+
+  class Zagata {
+    public $modx;
+    public $config = array();
+    function __construct(modX &$modx,array $config = array()) {
+        $this->modx =& $modx;
+ 
+        $basePath = $this->modx->getOption('zagata.core_path',$config,$this->modx->getOption('core_path').'components/zagata/');
+        $assetsUrl = $this->modx->getOption('zagata.assets_url',$config,$this->modx->getOption('assets_url').'components/zagata/');
+        $this->config = array_merge(array(
+            'basePath' => $basePath,
+            'corePath' => $basePath,
+            'modelPath' => $basePath.'model/',
+            'processorsPath' => $basePath.'processors/',
+            'chunksPath' => $basePath.'elements/chunks/',
+            'jsUrl' => $assetsUrl.'js/',
+            'cssUrl' => $assetsUrl.'css/',
+            'assetsUrl' => $assetsUrl,
+            'connectorUrl' => $assetsUrl.'connector.php',
+        ),$config);
+    }
+  }
+
+?>
